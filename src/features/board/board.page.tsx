8a7cae2cf8c844781bc48e34ca1488b8c3a841b1
Evgeny Paromov <evgeny.paromov@evrone.com>
2025-05-31 18:09:45 +0300

    feat: 18 2 render arrow nodes

--- src/features/board/board.page.tsx
+++ src/features/board/board.page.tsx
@@ -10,12 +10,12 @@ import { Overlay } from "./ui/overlay";
 import { Layout } from "./ui/layout";
 import { Dots } from "./ui/dots";
 import { Canvas } from "./ui/canvas";
-import { Sticker } from "./ui/sticker";
+import { Sticker } from "./ui/nodes/sticker";
 import { Actions } from "./ui/actions";
 import { ActionButton } from "./ui/action-button";
 import { useNodesDimensions } from "./hooks/use-nodes-dimensions";
 import { useWindowPositionModel } from "./model/window-position";
-import { Point, vectorFromPoints } from "./domain/point";
+import { Arrow } from "./ui/nodes/arrow";
 
 function BoardPage() {
   const nodesModel = useNodes();
@@ -52,13 +52,17 @@ function BoardPage() {
         onClick={viewModel.canvas?.onClick}
         windowPosition={windowPosition}
       >
-        {viewModel.nodes.map((node) => (
-          <Sticker key={node.id} {...node} ref={nodeRef} />
-        ))}
+        {viewModel.nodes.map((node) => {
+          if (node.type === "sticker") {
+            return <Sticker key={node.id} {...node} ref={nodeRef} />;
+          }
+          if (node.type === "arrow") {
+            return <Arrow key={node.id} {...node} ref={nodeRef} />;
+          }
+        })}
         {viewModel.selectionWindow && (
           <SelectionWindow {...viewModel.selectionWindow} />
         )}
-        <Arrow start={{ x: 30, y: 50 }} end={{ x: 90, y: 90 }} />
       </Canvas>
 
       <Actions>
@@ -77,37 +81,3 @@ function BoardPage() {
 }
 
 export const Component = BoardPage;
-
-function Arrow({ start, end }: { start: Point; end: Point }) {
-  const diff = vectorFromPoints(start, end);
-  const angle = Math.atan2(diff.y, diff.x);
-  const arrowRightAngle = angle + Math.PI * (1 - 1 / 6);
-  const arrowLeftAngle = angle - Math.PI * (1 - 1 / 6);
-  const arrowRightDiff = [
-    Math.cos(arrowRightAngle) * 10,
-    Math.sin(arrowRightAngle) * 10,
-  ];
-  const arrowLeftDiff = [
-    Math.cos(arrowLeftAngle) * 10,
-    Math.sin(arrowLeftAngle) * 10,
-  ];
-
-  return (
-    <svg className="absolute left-0 top-0 pointer-events-none">
-      <path
-        className="pointer-events-auto "
-        stroke="black"
-        strokeWidth={2}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d={`
-          M ${start.x} ${start.y} L ${end.x} ${end.y} 
-          M ${end.x} ${end.y} L ${end.x + arrowRightDiff[0]} ${end.y + arrowRightDiff[1]} 
-          L ${end.x + -5 * Math.cos(angle)} ${end.y + -5 * Math.sin(angle)}
-          L ${end.x + arrowLeftDiff[0]} ${end.y + arrowLeftDiff[1]}
-          L ${end.x} ${end.y}
-          `}
-      />
-    </svg>
-  );
-}
